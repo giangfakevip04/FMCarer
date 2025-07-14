3e20b8666e78f1038cf796ff834d55529f576d6b
Windows <giangtvph38936@fpt.edu.vn>
2025-07-14 11:26:36 +0700

    Thiết kế lại layout, sửa manifest, gradle build

--- app/src/main/java/com/example/fmcarer/network/response/LoginResponse.java
+++
@@ -1,9 +0,0 @@
-package com.example.fmcarer.network.response;
-
-public class LoginResponse {
-    private UserResponse user;
-    private String token;
-
-    public UserResponse getUser() { return user; }
-    public String getToken() { return token; }
-}
